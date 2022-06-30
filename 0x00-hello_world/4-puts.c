e91e186a49aa1e882b1d14965d0136c4504eebce
Felix Nzioki Savali <felixnzioki99@gmail.com>
2022-06-30 14:04:46 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char test_str[] = "\n\"Programming is like building a multilingual puzzle\n";
+	char test_str[] = "\n\"Programming is like building a multilingual puzzle";
 
 	puts(test_str);
 	return (0);
