1c7fbcc074a24ddbaaf0fae3499bb72321d5822d
Felix Nzioki Savali <felixnzioki99@gmail.com>
2022-06-30 14:30:44 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-/* Return: Always 0 (Success) */
 int main(void)
+	/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+	
 {
 	char text[] = "\n\"Programming is like building a multilingual puzzle";
 
 	puts(text);
+	
+	/* Return: Always 0 (Success) */
 	return (0);
 }
