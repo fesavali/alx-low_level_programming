0e3aa3fb41f23bd274e9bedeca0171b497530f37
Felix Nzioki Savali <felixnzioki99@gmail.com>
2022-06-30 14:46:55 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n/g");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	/* Return: Always 0 (Success) */
 	
 	return (0);
