82b965dc957e100cd8180b151dbab3e6ddf89243
Felix Nzioki Savali <felixnzioki99@gmail.com>
2022-07-01 15:39:09 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 /**
  * main - Entry point
  *Description: print all alphabets in small case
  * Return: Always 0 (Success)
- */ 
+ */
 int main()
 {
-    int x;
-    int y;
-    for(x = 'A'; x <= 'z'; x++) 
+ 
+    int ch;
+ 
+    for (ch = 'a'; ch <= 'z'; ch++)
     {
-        x = tolower(x);
-        putchar(x);
-        y = toupper(x);
-        putchar(y);
+        putchar(ch);
     }
-   
+    for (ch = 'A'; ch <= 'Z'; ch++)
+    {
+        putchar(ch);
+    }
+
+ 
     return 0;
 }
